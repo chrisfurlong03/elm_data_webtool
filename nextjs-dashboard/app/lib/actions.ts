27f1cff786368793cd63803880438e00072ecfd3
Chris Furlong <cfurlong@uchicago.edu>
2024-07-22 19:10:29 +0000

    Removed invoices from

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -2,18 +2,14 @@
 
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
-import { put } from '@vercel/blob';
-import { NextResponse } from 'next/server';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
 import axios from 'axios';
-import csv from 'csv-parser';
 import { Readable } from 'stream';
 import * as readline from 'readline';
-import { Buffer } from 'buffer';
-import { handleUpload, type HandleUploadBody } from '@vercel/blob/client';
+
 
  
 const FormSchema = z.object({
@@ -32,8 +28,7 @@ const FormSchema = z.object({
   date: z.string(),
 });
  
-const CreateInvoice = FormSchema.omit({ id: true, date: true });
-const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 
 // New schema
 
@@ -68,50 +63,11 @@ export type ELMState = {
   message?: string | null;
 };
 
-export async function createInvoice(prevState: State, formData: FormData) {
-  // Validate form using Zod
-  const validatedFields = CreateInvoice.safeParse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    lat: formData.get('lat'),
-    lon: formData.get('lon'),
-    startdt: formData.get('startdt'),
-    enddt: formData.get('enddt'),
-    status: formData.get('status'),
-  });
+
  
-  // If form validation fails, return errors early. Otherwise, continue.
-  if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors,
-      message: 'Missing Fields. Failed to Create Invoice.',
-    };
-  }
+
  
-  // Prepare data for insertion into the database
-  const { customerId, amount, lat, lon, startdt, enddt, status } = validatedFields.data;
-  const amountInCents = amount * 100;
-  const lonAbs = Math.abs(lon) * 100;
-  const latCents = lat * 100;
-  const date = new Date().toISOString().split('T')[0];
- 
-  // Insert data into the database
-  try {
-    await sql`
-      INSERT INTO invoices (customer_id, amount, lat, lon, startdt, enddt, status, date)
-      VALUES (${customerId}, ${amountInCents}, ${latCents}, ${lonAbs}, ${startdt}, ${enddt}, ${status}, ${date})
-    `;
-  } catch (error) {
-    // If a database error occurs, return a more specific error.
-    return {
-      message: 'Database Error: Failed to Create Invoice.',
-    };
-  }
- 
-  // Revalidate the cache for the invoices page and redirect the user.
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
-}
+
 
 export async function updateInputJob(
   id: string,
@@ -130,7 +86,7 @@ export async function updateInputJob(
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
-      message: 'Missing Fields. Failed to Update Invoice.',
+      message: 'Missing Fields. Failed to Update Input Job.',
     };
   }
  
@@ -143,24 +99,15 @@ export async function updateInputJob(
       WHERE id = ${id}
     `;
   } catch (error) {
-    return { message: 'Database Error: Failed to Update Invoice.' };
+    return { message: 'Database Error: Failed to Update Input Job.' };
   }
  
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+  revalidatePath('/dashboard/inputjobs');
+  redirect('/dashboard/inputjobs');
 }
 
 
-export async function deleteInvoice(id: string) {
 
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
-    return { message: 'Deleted Invoice.' };
-  } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice.' };
-  }
-}
 
 export async function deleteInputJob(id: string) {
 
@@ -344,7 +291,6 @@ export async function createInputJob(prevState: ELMState, formData: FormData) {
     };
   }
  
-  // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath('/dashboard/inputjobs');
   redirect('/dashboard/inputjobs');
 }
