29ce804d363eaa70b27e8e05cd3c15661c9b7e4d
Chris Furlong <cfurlong@uchicago.edu>
2024-07-24 01:53:49 +0000

    Disabled daymet method button and changed elm server url

--- nextjs-dashboard/app/ui/inputjobs/buttons.tsx
+++ nextjs-dashboard/app/ui/inputjobs/buttons.tsx
@@ -7,7 +7,8 @@ export function CreateInputJob() {
   return (
     <Link
       href="/dashboard/inputjobs/create"
-      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+      className="flex h-10 items-center rounded-lg bg-blue-300 px-4 text-sm font-medium text-white pointer-events-none"
+      aria-disabled={true}
     >
       <span className="hidden md:block">Create File</span>{' '}
       <PlusIcon className="h-5 md:ml-4" />
