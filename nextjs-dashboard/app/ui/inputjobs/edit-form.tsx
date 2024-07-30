7c7084614c373c8e7d2bbc940f0f4a7bc176f57f
Chris Furlong <cfurlong@uchicago.edu>
2024-07-30 18:43:25 +0000

    Starting some UI changes

--- nextjs-dashboard/app/ui/inputjobs/edit-form.tsx
+++ nextjs-dashboard/app/ui/inputjobs/edit-form.tsx
@@ -51,7 +51,7 @@ export default function EditInputJobForm({
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
-            Choose customer
+            User
           </label>
           <div className="relative">
             <select
@@ -77,18 +77,18 @@ export default function EditInputJobForm({
         {/* Invoice Amount */}
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
-            Choose an amount
+            Start year
           </label>
           <div className="relative mt-2 rounded-md">
             <div className="relative">
               <input
                 disabled
-                id="amount"
-                name="amount"
+                id="startdt"
+                name="startdt"
                 type="number"
                 step="0.01"
                 defaultValue={inputjob.startdt}
-                placeholder="Enter USD amount"
+                placeholder="Enter start year"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -99,7 +99,7 @@ export default function EditInputJobForm({
         {/* Invoice Status */}
         <fieldset disabled>
           <legend className="mb-2 block text-sm font-medium">
-            Set the invoice status
+            The input job status
           </legend>
           <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
             <div className="flex gap-4">
